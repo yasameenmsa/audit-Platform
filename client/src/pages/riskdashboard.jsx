e99df724a6b15f5250c702f5545a1099198c869e
Yasmeen Awawdeh (Yasmin MSA) <131165262+yasameenmsa@users.noreply.github.com>
2023-12-14 13:32:51 +0200

    dash

---
+++ client/src/pages/riskdashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DASHBOARDSIDEBAR from './DASHBOARD/DASHBOARDSIDEBAR';
+function riskdashboard() {
+  return (
+    <>
+    <div>riskdashboard</div>
+    <DASHBOARDSIDEBAR />
+    </>
+  )
+}
+
+export default riskdashboard
